3cf45a25c5bc36d912b8778ad769fb643154a4be
jakub.sveda <jakub.sveda@icloud.com>
2023-01-29 12:55:31 +0100

    Homework - currencies

--- python_intro/kurz.py
+++ python_intro/kurz.py
@@ -1,29 +1,27 @@
+from time import sleep
+
 import httpx
 from pick import pick
-from time import sleep
 
 URL = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt"
 data = httpx.get(URL).text.split("\n")
 data = data[2:-1]
-currencyJSON = {}
-currencies = []
+currency_json = {}
 for row in data:
     row = row.split("|")
-    currencyJSON.setdefault(row[3], float(row[4].replace(",", ".")) / int(row[2]))
-    currencies.append(row[3])
+    if row[3] > "CZK":
+        currency_json.setdefault("CZK", 1)
 
-currencyJSON.setdefault("CZK", 1)
-currencies.append("CZK")
-currencies.sort()
+    currency_json.setdefault(row[3], float(row[4].replace(",", ".")) / int(row[2]))
 
 
 def calculate_currency():
     before_currency_input_title = "Vyberte vstupní měnu: "
-    before_currency_input = pick(currencies, before_currency_input_title, "=>")
+    before_currency_input = pick(list(currency_json.keys()), before_currency_input_title, "=>")
     print(before_currency_input_title + before_currency_input[0])
 
     after_currency_input_title = "Vyberte výstupní měnu: "
-    after_currency_input = pick(currencies, after_currency_input_title, "=>")
+    after_currency_input = pick(list(currency_json.keys()), after_currency_input_title, "=>")
     print(after_currency_input_title + after_currency_input[0])
 
     if before_currency_input == after_currency_input:
@@ -43,7 +41,8 @@ def calculate_currency():
         sleep(2)
         return -1
 
-    calculation_result = input_amount * currencyJSON.get(before_currency_input[0]) / currencyJSON.get(after_currency_input[0])
+    calculation_result = input_amount * currency_json.get(before_currency_input[0]) / currency_json.get(
+        after_currency_input[0])
 
     return "Výsledek: " + str(calculation_result)
 
